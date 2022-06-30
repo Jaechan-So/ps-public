0103e484570298062663f5a801165c51dddd84de
Jaechan-So <jaechan331@gmail.com>
2022-06-30 23:00:19 +0900

    253

---
+++ problems/leetcode/0200/253.cpp
@@ -0,0 +1,36 @@
+// 253. Meeting Rooms II
+// https://leetcode.com/problems/meeting-rooms-ii/
+
+using namespace std;
+
+#include <queue>
+#include <vector>
+
+class Solution {
+ public:
+  int minMeetingRooms(vector<vector<int>>& intervals) {
+    priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>>
+        sortedIntervals;
+    for (auto& interval : intervals) {
+      sortedIntervals.push(interval);
+    }
+
+    priority_queue<int, vector<int>, greater<int>> idleMeetingRoomTimes;
+
+    while (!sortedIntervals.empty()) {
+      vector<int> currentInterval = sortedIntervals.top();
+      sortedIntervals.pop();
+
+      int start = currentInterval[0], end = currentInterval[1];
+      if (!idleMeetingRoomTimes.empty() &&
+          idleMeetingRoomTimes.top() <= start) {
+        idleMeetingRoomTimes.pop();
+        idleMeetingRoomTimes.push(end);
+      } else {
+        idleMeetingRoomTimes.push(end);
+      }
+    }
+
+    return idleMeetingRoomTimes.size();
+  }
+};
