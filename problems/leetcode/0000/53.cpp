b1888c5c274bb246a364c4c9af006ea5a36c9553
Jaechan-So <jaechan331@gmail.com>
2022-07-06 21:59:32 +0900

    53

---
+++ problems/leetcode/0000/53.cpp
@@ -0,0 +1,20 @@
+// 53. Maximum Subarray
+// https://leetcode.com/problems/maximum-subarray/
+
+using namespace std;
+
+#include <vector>
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int r = nums[0];
+
+        for(int i = 1, imax = r; i < nums.size(); i++){
+            imax = max(nums[i], imax + nums[i]);
+            r = max(r, imax);
+        }
+
+        return r;
+    }
+};
