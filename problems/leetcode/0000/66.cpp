f2d25db1c4488de420b4d94efb5bb8e2e61ea9bb
Jaechan-So <jaechan331@gmail.com>
2022-07-06 21:56:33 +0900

    66

---
+++ problems/leetcode/0000/66.cpp
@@ -0,0 +1,36 @@
+// 66. Plus One
+// https://leetcode.com/problems/plus-one/
+
+using namespace std;
+
+#include <vector>
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        vector<int> digitsRev;
+        bool carry = true;
+        for(int i = digits.size() - 1; i >= 0; i--){
+            if(carry){
+                if(digits[i] == 9){
+                    carry = true;
+                    digitsRev.push_back(0);
+                } else {
+                    carry = false;
+                    digitsRev.push_back(digits[i] + 1);
+                }
+            } else {
+                digitsRev.push_back(digits[i]);
+            }
+        }
+        if(carry){
+            digitsRev.push_back(1);
+        }
+
+        vector<int> digitsRevRev;
+        for(int i = 0; i < digitsRev.size(); i++){
+            digitsRevRev.push_back(digitsRev[digitsRev.size() - i - 1]);
+        }
+        return digitsRevRev;
+    }
+};
