d6d9ba3737523e47bb24d570660eca2ea2b33aab
Jaechan-So <jaechan331@gmail.com>
2024-10-01 14:30:56 +0900

    151

---
+++ problems/leetcode/0100/151.py
@@ -0,0 +1,6 @@
+# 151. Reverse Words in a String
+# https://leetcode.com/problems/reverse-words-in-a-string/description
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        return " ".join(reversed(s.split()))
